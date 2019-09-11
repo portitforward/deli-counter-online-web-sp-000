bfb256692a599cb2294be925836adaec9b7b37d7
Cody Fussell <cody.fussell@gmail.com>
2019-09-11 00:21:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,26 @@
 katz_deli = []
 
-def line()
+def line(tracker)
+    line_str = "The line is currently: "
+    if tracker.length == 0
+        return "The line is currently empty."
+    else
+        tracker.each_with_index{|v, i| line_str += "#{i+1}. #{v} "}
+        return line_str.chomp(" ") 
+    end          
 end
 
-def 
+def take_a_number(tracker, name)
+    tracker.push(name)
+    puts "Welcome, #{name}. You are currently number #{tracker.length} in line."
+    
+end
+
+def now_serving(tracker)
+    if tracker.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{tracker[0]}."
+        tracker.shift()
+    end    
+end
