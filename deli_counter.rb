7fc093fb44385648c47fa8b31e178a245162f685
Cody Fussell <cody.fussell@gmail.com>
2019-09-11 00:22:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(tracker)
+    line_str = "The line is currently: "
+    if tracker.length == 0
+        puts "The line is currently empty."
+    else
+        tracker.each_with_index{|v, i| line_str += "#{i+1}. #{v} "}
+        puts line_str.chomp(" ") 
+    end          
+end
+
+def take_a_number(tracker, name)
+    tracker.push(name)
+    puts "Welcome, #{name}. You are number #{tracker.length} in line."
+    
+end
+
+def now_serving(tracker)
+    if tracker.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{tracker[0]}."
+        tracker.shift()
+    end    
+end
